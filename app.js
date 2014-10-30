dfd49c01bda1bb734da3053beb46dffe02244e9a
MPMoughan <MattM@AmandoBs-MacBook-Pro-2.local>
2014-10-30 16:42:36 -0700

    updated app and Procfile

--- app.js
+++ app.js
@@ -315,6 +315,6 @@ app.get('*', function (req,res){
   res.render('404',{user:req.user});
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
